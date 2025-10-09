65a09dc81fd57ec78f36092bf7289d94cded52d0
JE.Ravshanov <JE.Ravshanov@SEVRU.ngmk.uz>
2025-10-09 15:51:32 +0500

    update cors5

--- src/main.ts
+++ src/main.ts
@@ -8,31 +8,31 @@ import * as path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-// --- 1. Создаём папку uploads, если нет ---
+  // --- 1. Создаём папку uploads, если нет ---
   const uploadsDir = path.join(__dirname, '..', 'uploads');
   if (!fs.existsSync(uploadsDir)) {
     fs.mkdirSync(uploadsDir, { recursive: true });
     console.log('📁 Папка uploads создана автоматически');
   }
 
-  // --- 2. Раздаём файлы из uploads ---
-  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads',
-  });
-
   // Получаем native Express instance и сохраняем в global
   const expressApp = app.getHttpAdapter().getInstance();
   // безопасно присваиваем глобально
   (global as unknown as { express?: import('express').Express }).express = expressApp;
 
+  app.useStaticAssets(join(__dirname, '..', 'uploads'), {
+    prefix: '/uploads/',
+  });
 
   app.enableCors({
     origin:'*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
 
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 Server running on port ${process.env.PORT || 3000}`);
 }
 bootstrap();
